2db7fd180c961daca07c4b0083d78e15ad77ff2c
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-01-23 02:44:06 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,12 +1,6 @@
 function solution(arr) {
-    var answer = 0;
-    var sum = 0;
     
-    for(let i =0; i < arr.length; i++) {
-        sum += arr[i];
-    }
+    const sum = arr.reduce((a,b) => a + b, 0);
     
-    answer = sum / arr.length
-
-    return answer;
+    return sum / arr.length;
 }
