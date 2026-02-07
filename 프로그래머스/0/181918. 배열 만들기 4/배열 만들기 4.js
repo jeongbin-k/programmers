8d39a468002e70dab95871a923a13b78952fcb4e
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-07 15:23:16 +0900

    [level 0] Title: 배열 만들기 4, Time: 30.15 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -3,17 +3,9 @@ function solution(arr) {
     let i = 0;
     
     while(i < arr.length) {
-        if(stk.length === 0) {
-            stk.push(arr[i])
-            i++
-        }
-        else if (stk[stk.length-1] < arr[i]) {
-            stk.push(arr[i])
-            i++
-        }
-        else {
-            stk.pop()
-        }
+        if(stk.length === 0) stk.push(arr[i++]);
+        else if (stk[stk.length-1] < arr[i]) stk.push(arr[i++]);
+        else stk.pop();
     }
         
     return stk;
