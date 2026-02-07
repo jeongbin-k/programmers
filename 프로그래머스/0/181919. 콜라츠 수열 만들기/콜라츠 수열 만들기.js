068be6e869257edebfe4720280fa7885bf675972
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-07 14:49:51 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let result = [];
+
+  while (n !== 1) {
+    result.push(n);
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+  }
+  result.push(1);
+  return result;
+}
