067353aab342e480d0a1ee77f3a2c09b2aa7ff77
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-07 15:29:19 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.68 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -3,14 +3,13 @@ function solution(arr) {
     let i = 0;
     
     while(i < arr.length) {
-        if(stk.length === 0) stk.push(arr[i++]);
-        else if (stk[stk.length-1] < arr[i]) stk.push(arr[i++]);
-        else stk.pop();
-    }
-        
+        if(stk.length === 0 || stk[stk.length-1] < arr[i]) {
+            stk.push(arr[i++])
+        }else {
+            stk.pop()
+        }
+    }    
     return stk;
-    
-    
     
     // 규칙 1. 빈 배열이면 arr[i] stk에 추가 i증가
     // 규칙 2. stk의 마지막 원소 < arr[i] => arr[i]를 뒤에 추가 i증가
