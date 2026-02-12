0cfaa7a16cfc67f998512b7eb1c352d203a9cb98
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-13 03:25:09 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+    let suffix = [];
+    
+    for(let i = 0; i < my_string.length; i++) {
+        suffix.push(my_string.slice(i));
+    }
+    
+    return (suffix.includes(is_suffix)) ? 1 : 0
+}
