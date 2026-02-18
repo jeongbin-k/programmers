d3edcf0398c72fdeec0e387055773b76dfa6e5cb
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-18 22:20:26 +0900

    [level 0] Title: 글자 지우기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -0,0 +1,10 @@
+function solution(my_string, indices) {
+    let result = [];
+    
+    for(let i = 0; i < my_string.length; i++) {
+        if(!indices.includes(i)) {
+            result += my_string[i];
+        }
+    }
+    return result;
+}
