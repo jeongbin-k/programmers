67fb019bcaaed7b7b3f3893c9e27f28d3dd0a6bd
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-23 17:33:32 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,3 +1,10 @@
 function solution(num_list, n) {
     return num_list.slice(0,n);
+    
+    // for문
+    // let result = []
+    // for(let i = 0; i < n; i++) {
+    //     result.push(num_list[i]);
+    // }
+    // return result;
 }
