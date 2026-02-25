fbb49f871037bb08683000d2a9b486cdb5143a9a
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-25 17:02:35 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,5 +1,3 @@
 function solution(num_list, n) {
-    const after = num_list.slice(n);
-    const before = num_list.slice(0, n); 
-    return after.concat(before);
+    return num_list.slice(n).concat(num_list.slice(0,n));
 }
