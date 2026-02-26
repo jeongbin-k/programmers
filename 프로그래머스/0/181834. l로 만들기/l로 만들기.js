72d727d41fad52a050bc0a1cbcd5fb19db4707e2
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-02-26 16:09:46 +0900

    [level 0] Title: l로 만들기, Time: 6.41 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,8 +1,1 @@
-function solution(myString) {
-    let result = "";
-    
-    for(let i = 0; i < myString.length; i++) {
-        result += (myString[i] < "l" ? "l" : myString[i]);
-    }
-        return result;
-}
+const solution =(myString) => myString.replace(/[a-k]/g, 'l')
