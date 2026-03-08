1318f25c23ed4d24bd94144d3fde640a97db517c
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-03-08 16:45:24 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let a = 0;
+    let b = 0;
+    
+    for(let i = 0; i < num_list.length; i++) {
+        if(i % 2 === 0) {
+            a += num_list[i];
+        } else {
+            b += num_list[i];
+        }
+    }
+    
+    return Math.max(a,b);
+}
