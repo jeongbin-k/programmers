6ca15530e35edaf12175bbaf536cbf138af459b8
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-03-09 16:17:00 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.22 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,8 +1,5 @@
 function solution(arr, intervals) {
-    const [[a1, b1], [a2, b2]]  = intervals;
+    const [[a1, b1], [a2, b2]] = intervals;
     
-    const first = arr.slice(a1, b1 + 1);
-    const second = arr.slice(a2, b2 + 1);
-    
-    return [...first, ...second];
+    return [...arr.slice(a1, b1 + 1), ...arr.slice(a2, b2 + 1) ];
 }
