73e8fbf7de98403dda76e7f3c23eadca4b16d96b
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-03-10 15:33:52 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers, n) {
+    let sum = 0;
+    
+    for(let num of numbers) {
+        sum += num;
+        
+        if(sum > n) {
+            return sum;
+        }
+    }
+}
