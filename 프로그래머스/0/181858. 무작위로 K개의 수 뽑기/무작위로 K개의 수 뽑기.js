8f65220579cd7198699c5501db37e64bb4ce2db2
Jeongbin Kang <101164866+jeongbin-k@users.noreply.github.com>
2026-04-14 16:37:15 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.35 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,19 @@
+function solution(arr, k) {
+    let result = [];
+    const seen = new Set();
+    
+    for(const num of arr) {
+        if(!seen.has(num)) {
+            seen.add(num);
+            result.push(num);
+        }
+        
+        if(result.length === k) break;
+    }
+    
+    while (result.length < k) {
+        result.push(-1);
+    }
+    
+    return result;
+}
